eac1c3a69c9f95f4313a47048d964d5abeb69bae
Fabio Camargos <fabiocmmcm@gmail.com>
2021-06-11 18:07:42 -0300

    reqs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
+const { PORT } = process.env
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -15,8 +17,9 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
+const UPSIDEDOWN = process.env.UPSIDEDOWN_MODE;
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = UPSIDEDOWN;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
