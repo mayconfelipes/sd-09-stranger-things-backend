95b568b1a9196c9c60c924dc0839709461ec1e9b
Mario Pardo <mariio.pardojr@gmail.com>
2021-07-12 19:07:13 -0300

    Alteracoes index

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+// const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
