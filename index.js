0456b0e01e667724b270ef180f7524f630f1c519
matheusjkl1 <matheus.unilog@gmail.com>
2021-07-22 17:09:28 -0300

    hawkins

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -14,9 +15,13 @@ const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
 
-app.use(cors());
+let hereIsTheUpsideDown = false;
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+if (UPSIDEDOWN_MODE === 'true') {
+  hereIsTheUpsideDown = true;
+}
 
-const hereIsTheUpsideDown = true;
+app.use(cors());
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +32,7 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
+  console.log(hereIsTheUpsideDown);
   console.log('Escutando na porta 3000');
 });
