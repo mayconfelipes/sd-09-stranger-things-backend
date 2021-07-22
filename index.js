509f22d21ad7f56db6af62e74bdddfeab59287f5
jdaniel-ap <jdaniell.93@gmail.com>
2021-07-22 17:30:04 -0300

    env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE || 'false';
+const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE || false;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
