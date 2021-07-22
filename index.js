ec3a55aa0b09c588eff9db58aca183c66eff4b5e
Matheus Martino <matheus.martino97@gmail.com>
2021-07-22 20:41:18 -0300

    hdfk

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.use(cors());
 
 const hereIsTheUpsideDown = true;
 
-console.log(process.env.UPSIDEDOWN_MODE);
-
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
