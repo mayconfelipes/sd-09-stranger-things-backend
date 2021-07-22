3d70d1415ff97cfd9e4df19012a69a5cb0cfa934
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-07-22 15:40:13 -0300

    Add procfile e ecosystem

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+
+const { PORT, UPSIDEDOWN_MODE } = process.env;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,7 +19,11 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+console.log(UPSIDEDOWN_MODE);
+
+const hereIsTheUpsideDown = (UPSIDEDOWN_MODE === 'true');
+
+console.log(hereIsTheUpsideDown);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +34,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
