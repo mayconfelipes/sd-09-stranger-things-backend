5879471b5abb9b30c6c5a1a792bff31fc04fa866
Douglas Ferreira <dodofp.df@gmail.com>
2021-07-22 17:06:42 -0300

    Adicionando Number ao PORT

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -11,13 +13,13 @@ const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
 const strangerThingsService = new StrangerThingsService(
-  
+
   strangerThingsRepository,
 );
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = (process.env.UPSIDEDOWN_MODE === 'true');
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -28,6 +30,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Escutando na porta ${process.env.PORT}`);
+const PORT = Number(process.env.PORT);
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
