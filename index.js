52f72987bd6e7579094265fbd9a445248da7c8ab
wagnerberna <cabelo29007@gmail.com>
2021-07-22 14:17:12 -0300

    projeto feito na T08

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
+// REQ01
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+// FIM.
+
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
@@ -15,8 +22,6 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
-
-const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +32,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
