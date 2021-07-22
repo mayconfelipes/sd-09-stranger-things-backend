670189ae9d2de55aa476c8fee90e000f064a4ba5
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-07-22 16:34:31 -0300

    Começando o projeto regnarts sgniht frontend

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -6,17 +7,18 @@ const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
+const PORT = process.env;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
-);
-const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
-);
+  );
+  const strangerThingsService = new StrangerThingsService(
+    strangerThingsRepository,
+    );
+
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.use(cors());
-
-const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
