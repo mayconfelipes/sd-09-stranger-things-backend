4d0872d8b4211b08e2f90133fc0be67930be2642
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-07-22 14:39:14 -0300

    feat/requirement 1

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -16,6 +17,10 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
+const PORT = process.env.PORT || 3000;
+
+console.log(process.env.UPSIDEDOWN_MODE);
+
 const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
@@ -27,6 +32,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
