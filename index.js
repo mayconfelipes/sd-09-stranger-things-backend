35d1c16b0aa8add0a174ae0638f82dcbc567075b
Bruno Wedley <brunowedley27@gmail.com>
2021-07-22 16:38:27 -0300

    alteracao em variavel de ambiente

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE ? 'true' : false;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
