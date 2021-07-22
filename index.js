4566b463c36b5e1c6cd5047e7274f296eb1ba7c5
jdaniel-ap <jdaniell.93@gmail.com>
2021-07-22 17:15:59 -0300

    fixing err

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
+const { PORT } = process.env;
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -16,7 +18,9 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+let hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+
+hereIsTheUpsideDown = hereIsTheUpsideDown !== 'false';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +31,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
