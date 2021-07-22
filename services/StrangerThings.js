bf3f548040c4445b2da4ffbdf9bdd11af0d47bd0
tiago-yoneda <xyonedax@gmail.com>
2021-07-22 14:59:03 -0300

    tentando fix v2

--- services/StrangerThings.js
+++ services/StrangerThings.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const flipout = require('flipout');
-
+require('dotenv').config();
 class StrangerThingsService {
   constructor(repository) {
     this.repository = repository;
@@ -9,17 +9,14 @@ class StrangerThingsService {
 
   search({ page, size, ...params }, upsideDownMode) {
     const characters = this.repository.search(params, { page, size });
-    console.log('###');
+    console.log('####');
     console.log(upsideDownMode);
     if (upsideDownMode) {
+      console.log('entrou na funcao')
       return characters.map(({ name, origin, status }) => ({
-        name: 'não invertido',
+        name: flipout(name),
         origin: flipout(origin),
         status: flipout(status),
-
-        // name: flipout(name),
-        // origin: flipout(origin),
-        // status: flipout(status),
       }));
     }
 
