18d6c69dbd3a23b2c0db6f4922cb81bd3783ef5e
Osvaldo Borges <netoo70@gmail.com>
2021-07-22 17:24:20 -0300

    Heroku

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true' || false;
 const PORT = process.env.PORT || 3000;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true' || false;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
