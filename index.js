7073b23c270c268d9f83a7f875b55c1fe8021f3c
Matheus Martino <matheus.martino97@gmail.com>
2021-07-22 20:05:36 -0300

    Adicionando PORT como variavel de ambiente.

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+  console.log(`Escutando na porta`);
 });
