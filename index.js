885db2259daf20e403baac59ef87fefdb9ef20b4
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-07-22 17:36:33 -0300

    Heroku

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
-const { PORT } = process.env;
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+
+const convertToBoolean = (string) => {
+  if (string === 'true') return true;
+  return false;
+};
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -19,7 +24,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = convertToBoolean(UPSIDEDOWN_MODE);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
