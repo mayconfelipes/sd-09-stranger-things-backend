1caca2820b8eb94740e167e3289dbe4dff6157e0
Vagner Pereira <sousa.vagner82@gmail.com>
2021-07-23 15:29:28 -0300

    requisito 1

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log('Escutando na porta 3000');
 });
