0bb6e87e978aaf86ef736b7d25d7203508b816d6
Carine Becker <caarine.m@gmail.com>
2021-07-23 12:21:25 -0300

    fixes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 console.log(process.env.UPSIDEDOWN_MODE);
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
