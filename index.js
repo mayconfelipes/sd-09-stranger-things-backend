e5ab6ea004c2c412f9ba5ae631967ddf7e236c3a
Vagner Pereira <sousa.vagner82@gmail.com>
2021-07-23 17:40:08 -0300

    variavel ambiente

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -30,5 +30,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('Escutando na porta 3000');
+  console.log(`Escutando na porta ${port}`);
 });
