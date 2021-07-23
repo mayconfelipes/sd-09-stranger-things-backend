821019a48f1a594061f6197e1fecf8c986e6534e
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-23 19:16:56 -0300

    criando e configurando variaveis de ambiente

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv/config');
+//  gerenciando variaveis de ambiente nodeJS
+//  https://blog.rocketseat.com.br/variaveis-ambiente-nodejs/
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,19 +19,20 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+// const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    process.env.UPSIDEDOWN_MODE,
   );
 
   res.status(200).json(characters);
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
 
@@ -36,7 +40,7 @@ app.listen(port, () => {
 // passo 2: cd path_sd-09-stranger-things-backend
 // passo 3: heroku login
 // passo 4: git checkout -b adrianoforcellini-strangerthings
-// passo 5: npm i expres's
+// passo 5: npm i express
 // passo 6: heroku create adriano-stranger-heroku
 // passo 7: git commit -am 'iniciando deploy'
-// passo 8: git push heroku adrianoforcellini-strangerthings
+// passo 8: git push heroku adrianoforcellini-strangerthings:main
