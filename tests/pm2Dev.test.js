5692eb020d83f056cedf64c29093b6dd07c624a2
Matheus Martino <matheus.martino97@gmail.com>
2021-07-23 09:27:46 -0300

    Adicionando Procfil.

--- tests/pm2Dev.test.js
+++ tests/pm2Dev.test.js
@@ -1,6 +1,6 @@
 const { dependencies } = require('../package.json');
 
-describe.skip('4 - Verifica se o módulo pm2 foi instalado no projeto', () => {
+describe('4 - Verifica se o módulo pm2 foi instalado no projeto', () => {
   it('Será validado que o modulo pm2 está instalado nas dependências', () => {
     expect(dependencies).toHaveProperty('pm2');
   });
