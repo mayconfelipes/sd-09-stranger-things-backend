a636dd228994e697c1e9f37eec365871c68a447b
edison-ps <edison@osite.com.br>
2021-07-23 15:52:37 -0300

    Requisito 1 V1.00

--- index.js
+++ index.js
@@ -14,9 +14,15 @@ const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
 
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+let hereIsTheUpsideDown = false;
+
+if (UPSIDEDOWN_MODE === 'true') {
+  hereIsTheUpsideDown = true;
+}
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +33,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
