98343f06d1c6e53ae9fe4aea9c3fd3e8959bf5a4
Matheus Martino <matheus.martino97@gmail.com>
2021-07-22 22:10:18 -0300

    Iniciando heroku.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
     hereIsTheUpsideDown,
   );
 
-  res.status(200).json({ characters });
+  res.status(200).json(characters);
 });
 
 const PORT = process.env.PORT || 3000;
