046bb57b4cbdaac7a28a8d848087ade3a379ee4f
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-07-23 23:10:11 -0300

    req 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -16,7 +17,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || true;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,7 +29,7 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
 
