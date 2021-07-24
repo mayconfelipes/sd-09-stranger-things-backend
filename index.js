ab49dab466fc6bb273dadb9a12d30c027b36c69c
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-07-23 23:33:28 -0300

    fals

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || false;
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
