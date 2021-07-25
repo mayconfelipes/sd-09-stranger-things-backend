9377ed5ce40b624bd9858ada459c015582cbdc01
Raquel Picanço <picanco.raquel@gmail.com>
2021-07-25 12:29:33 -0400

    fix upsidedown again

--- services/StrangerThings.js
+++ services/StrangerThings.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const flipout = require('flipout');
-const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE;
 
 class StrangerThingsService {
   constructor(repository) {
@@ -11,7 +10,7 @@ class StrangerThingsService {
   search({ page, size, ...params }, upsideDownMode) {
     const characters = this.repository.search(params, { page, size });
 
-    if (UPSIDEDOWN_MODE) {
+    if (upsideDownMode) {
       return characters.map(({ name, origin, status }) => ({
         name: flipout(name),
         origin: flipout(origin),
