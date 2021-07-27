78c6f948dbc147031add0573b2381d1feb5bc0b0
marcusWittho <witthoeft.mcs@hotmail.com>
2021-07-27 14:12:12 -0300

    Alteração do .env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-// const UPSIDEDOWN_MODE = process.env.UPSIDEDOWN_MODE;
+const { UPSIDEDOWN_MODE } = process.env;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -19,7 +19,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
