6150897a4885ec74a93c9d555f5cccfbee705f46
Hudson Carlos <hc88806@gmail.com>
2021-07-27 13:15:00 -0400

    fixing lint

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 
-
 const PORT = process.env.PORT || 3000;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
