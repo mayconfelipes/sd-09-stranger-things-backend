0051f9fbdb9655ff3ee0ef404390665b49266473
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-07-27 19:24:04 -0300

    testing commit2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv/config');
 
-const UPSIDEDOWN_MODE = Boolean(process.env.UPSIDEDOWN);
+const UPSIDEDOWN_MODE = Boolean(process.env.UPSIDEDOWN_MODE);
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
