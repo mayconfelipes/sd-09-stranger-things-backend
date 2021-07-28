7b446a96ed346c6a27d9e713d8ab58b57e7247e5
Joao Victor <30628255+Galaraz@users.noreply.github.com>
2021-07-28 15:34:27 -0300

    agora sim 2.0

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
-const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
