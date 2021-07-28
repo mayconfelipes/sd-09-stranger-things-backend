b23530ae7424d9842d0545021b87e540c364d90a
Joao Victor <30628255+Galaraz@users.noreply.github.com>
2021-07-28 19:24:54 -0300

    supostamente agora vai

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv/config');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsService = require('./services/StrangerThings');
