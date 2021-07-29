8942e40ffca6caa8e740a23229a70a7c8f3fbb7e
Pablo Fernandes <pabloprates@gmail.com>
2021-07-28 21:30:34 -0300

    Adding the ecosystem file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 require('dotenv/config');
+
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
