7c4f86a1fdca0e1a4c4ad77e330bbc7206bbfb1e
julianafatsil <julianafatsil@gmail.com>
2021-07-28 21:22:33 -0300

    enviroment variables configuration and validation trasnformationBoolean

--- index.js
+++ index.js
@@ -15,8 +15,15 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
+const { PORT, UPSIDEDOWN_MODE } = process.env;
+const hereIsTheUpsideDown = transformationBoolean(UPSIDEDOWN_MODE);
 
-const hereIsTheUpsideDown = true;
+const transformationBoolean = () => {
+  if (UPSIDEDOWN_MODE === 'false') {
+    return false
+  }
+  return true;
+}
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +34,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen( PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
