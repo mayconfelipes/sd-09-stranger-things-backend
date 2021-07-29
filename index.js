28439f541bcec4ccf8ffe4552caf22c25b8ba31b
Felipe Rocha <felipe.sr27@gmail.com>
2021-07-29 17:23:01 -0300

    fixed lint issue

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 const { PORT, UPSIDEDOWN_MODE } = process.env;
 
-const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true' ? true : false;
+const hereIsTheUpsideDown = (UPSIDEDOWN_MODE === 'true');
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
