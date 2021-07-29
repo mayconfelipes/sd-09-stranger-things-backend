d2df05ff5da658af22df16971711a47c3124e6c6
fomes <contato.felipeluna@gmail.com>
2021-07-29 10:23:03 -0300

    fix port index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const { UPSIDEDOWN_MODE } = process.env;
 
 const app = express();
