4357d50b1b0254262a99c8476ad2c7621bea4490
RodrigoNeoEx <draja.acc@gmail.com>
2021-08-01 13:15:56 -0300

    linter + changed bool check form

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -17,8 +17,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
