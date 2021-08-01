fc46ecf72fabdec441d0a4d48c464c0f15c1d88e
Daniel Cespedes <dev.danielcespedes@gmail.com>
2021-08-01 20:41:00 -0300

    alterando o process.env

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const { PORT, UPSIDEDOWN_MODE } = process.env;
+const PORT = process.env.PORT || '3000'
 
-const hereIsTheUpsideDown = Boolean(UPSIDEDOWN_MODE);
+const hereIsTheUpsideDown = Boolean(process.env.UPSIDEDOWN_MODE);
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -30,6 +30,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Escutando na porta ${process.env.PORT}`);
 });
