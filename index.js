d2e078d2cd2fc2f90ede7fee9d9d0da8dc013bd6
Daniel Cespedes <dev.danielcespedes@gmail.com>
2021-08-03 15:00:57 -0300

    alterando o upsidedown_mode para true ou false

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || '3000';
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
