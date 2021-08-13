ce06d31315c50741532dddee4c5b3f93a4bdb629
cleber-teixeira <cleberlt@gmail.com>
2021-08-12 21:13:19 -0300

    Requisito 06 - Refatoraçao3. :white_check_mark:

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true' || false;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
